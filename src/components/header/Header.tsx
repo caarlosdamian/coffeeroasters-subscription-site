6aaaee2431107c14a16ae289783f766e1c1b6d25
caarlosdamian <dapecarlos@gmail.com>
2023-04-22 22:21:03 -0600

    feat(header): auto close menu implemented

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,11 +1,18 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { hamburger, logo } from '../../assets';
 import { Menu } from '../menu/Menu';
 import { close } from '../../assets';
 import './Header.scss';
+import { useLocation } from 'react-router-dom';
 
 export const Header = () => {
   const [show, setShow] = useState<boolean>(false);
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    setShow(false);
+  }, [pathname]);
+
   return (
     <nav className="header">
       {show && <Menu />}
